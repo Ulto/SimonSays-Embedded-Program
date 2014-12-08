f66b5f52a941b3fa55dcdf25616b6120f6629fc4
Zach Cross <me@zachcross.com>
2014-12-08 14:09:10 -0500

    Initial Commit of code written during lab period.

---
+++ GamePlayFunctions.c
@@ -0,0 +1,36 @@
+// CMPET 456
+// Final Lab - Fall 2014
+// Simon Game
+// ---------------------
+// Zach Cross
+// Greg Pohodich
+//
+// Gameplay Functions
+
+
+// Record New Pattern Based On User Input
+void SetPattern(void)
+{
+
+} // END SetPattern
+
+
+// Play Back Current Pattern
+void PlayPattern(void)
+{
+
+} // END PlayPattern
+
+
+// Send Current Pattern To Remote Host
+void TxPattern(void)
+{
+
+} // END TxPattern
+
+
+// Recieve Pattern From Remote Host and Store As Current Psttern
+void RxPattern(void)
+{
+
+} // END RxPattern

9d317150f5b55479cc487a3d78bb7ebc9c9dc5e2
Zach Cross <me@zachcross.com>
2014-12-10 22:35:02 -0500

    Added DEFINES Standardized DEFINE length Added GamePlayFunction Prototypes

--- Main.h
+++ Main.h
@@ -4,23 +4,32 @@
 
 
 // DEFINES
-#define MAX_PATTERN_LENGTH 	25
-#define MAX_ROUNDS			10
+// Internal Constants
+#define MAX_PATTERN_LENGTH 		25
+#define MAX_ROUNDS				10
+#define PATTERN_PLAYBACK_DELAY	250		// ms
+#define TCP_PORT				1500
+#define ACTIVE					1
 
-// Assign pins
-#define	RED_OUT		0
-#define GREEN_OUT	1
-#define BLUE_OUT	2
+// LED Output(s)
+#define	RED_LT		0
+#define GRN_LT		1
+#define BLU_LT		2
+#define YEL_LT		3
 
-#define RED_PB		3
-#define GREEN_PB	4
-#define BLUE_PB		5
-#define YELLOW_PB	6
-
+// Push-button Input(s)
+#define RED_PB		4
+#define GRN_PB		5
+#define BLU_PB		6
+#define YEL_PB		7
 
 
 // FUNCTION PROTOTYPES
 void PrintHeader(void);
+void PlayPattern(void);
+void TxPattern(void);
+void RxPattern(void);
+void Delay_ms(int);
 
 
 // GLOBAL VARIABLES
@@ -29,9 +38,7 @@ struct game_data_struct
 	char 	pattern[MAX_PATTERN_LENGTH];
 	int 	round_num;
 
-
 } game_data;
-
 
 
 #endif /* MAIN_H */

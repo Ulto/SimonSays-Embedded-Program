e907432ba677c047c8e3b140dc123b1c6a4bcbc0
Zach Cross <me@zachcross.com>
2014-12-12 02:29:57 -0500

    Update GamePlayFunctions.c

--- GamePlayFunctions.c
+++ GamePlayFunctions.c
@@ -10,74 +10,78 @@
 #include "Main.h"
 
 // Record New Pattern Based On User Input
-void SetPattern(void)
+void SetPattern(struct game_data_struct *game_data)
 {
 	int loop_ctr;
 
 	// Re-Iterate pattern capture length to match round number.
-	for (loop_ctr = 0; loop_ctr <= game_data.round_num; loop_ctr++)
+	for (loop_ctr = 0; loop_ctr < game_data->round_num; loop_ctr++)
 	{
 
-		// Light LED (white) to indicate a pattern step "record mode"
+		// Light all LEDs to indicate a pattern step "record mode"
 		Write_PortB(RED_LT, 1);
 		Write_PortB(GRN_LT, 1);
 		Write_PortB(BLU_LT, 1);
-		Write_PortB(YEL_LT, 0);
+		Write_PortB(YEL_LT, 1);
 
 		// Block, then save and show input color.
-		while((RED_PB != ACTIVE) & (RED_PB != ACTIVE) & (RED_PB != ACTIVE) & (RED_PB != ACTIVE))
+		while((Read_PortB(RED_PB) != ACTIVE) & (Read_PortB(GRN_PB) != ACTIVE) & (Read_PortB(BLU_PB) != ACTIVE) & (Read_PortB(YEL_PB) != ACTIVE))
 		{
-			if (RED_PB == ACTIVE);
+			Delay_ms(5);
+		}
+
+
+			if (Read_PortB(RED_PB) == ACTIVE)
 			{
-				game_data.pattern[loop_ctr] = 'R';
+				printf("RED_PB\n");
+				game_data->pattern[loop_ctr] = 'R';
 				Write_PortB(RED_LT, 1);
 				Write_PortB(GRN_LT, 0);
 				Write_PortB(BLU_LT, 0);
 				Write_PortB(YEL_LT, 0);
-				break;
 			}
-
-			if (GRN_PB == ACTIVE);
+			else if (Read_PortB(GRN_PB) == ACTIVE)
 			{
-				game_data.pattern[loop_ctr] = 'G';
+				printf("GRN_PB\n");
+				game_data->pattern[loop_ctr] = 'G';
 				Write_PortB(RED_LT, 0);
 				Write_PortB(GRN_LT, 1);
 				Write_PortB(BLU_LT, 0);
 				Write_PortB(YEL_LT, 0);
-				break;
 			}
-
-			if (BLU_PB == ACTIVE);
+			else if (Read_PortB(BLU_PB) == ACTIVE)
 			{
-				game_data.pattern[loop_ctr] = 'B';
+				printf("BLU_PB\n");
+				game_data->pattern[loop_ctr] = 'B';
 				Write_PortB(RED_LT, 0);
 				Write_PortB(GRN_LT, 0);
 				Write_PortB(BLU_LT, 1);
 				Write_PortB(YEL_LT, 0);
-				break;
 			}
-
-			if (YEL_PB == ACTIVE);
+			else if (Read_PortB(YEL_PB) == ACTIVE)
 			{
-				game_data.pattern[loop_ctr] = 'Y';
+				printf("YEL_PB\n");
+				game_data->pattern[loop_ctr] = 'Y';
 				Write_PortB(RED_LT, 0);
 				Write_PortB(GRN_LT, 0);
 				Write_PortB(BLU_LT, 0);
 				Write_PortB(YEL_LT, 1);
-				break;
+			}
+			else
+			{
+
 			}
 
 			// Hold to show saved color.
 			Delay_ms(500);
-		}
-
+		
 	}
 
 } // END SetPattern
 
 
 // Play Back Current Pattern
-void PlayPattern(void)
+void PlayPattern(struct game_data_struct *game_data)
 {
 	int loop_ctr;
 
@@ -87,7 +91,7 @@ void PlayPattern(void)
 		Delay_ms(PATTERN_PLAYBACK_DELAY);
 
 		// Turn On Relevant Color
-		switch(game_data.pattern[loop_ctr])
+		switch(game_data->pattern[loop_ctr])
 		{
 			case 'R':
 				Write_PortB(RED_LT, 1);
@@ -127,7 +131,7 @@ void PlayPattern(void)
 
 
 // Send Current Pattern To Remote Host
-void TxPattern(void)
+void TxPattern(struct game_data_struct *game_data)
 {
 	// Send game_data struct to remote host.
 	NtwkSend(sizeof(game_data), game_data);
@@ -136,93 +140,92 @@ void TxPattern(void)
 
 
 // Recieve Pattern From Remote Host and Store As Current Psttern
-void RxPattern(void)
+void RxPattern(struct game_data_struct *game_data)
 {
-
-	// Wait For Connection
-	NtwkWait();
-
 	// Recieve game_data struct from remote host.
 	NtwkRecv(sizeof(game_data), game_data);
 
 } // END RxPattern
 
-void PlayerRepeatPattern(void)
+
+//
+void PlayerRepeatPattern(struct game_data_struct *game_data)
 {
 	char Repeatpattern[MAX_PATTERN_LENGTH] = { 0 };
 	char loop_ctr;
 
 	// Re-Iterate pattern capture length to match round number.
-	for (loop_ctr = 0; loop_ctr <= game_data.round_num; loop_ctr++)
+	for (loop_ctr = 0; loop_ctr <= game_data->round_num; loop_ctr++)
 	{
 
 		// Block, then save and show input color.
-		while ((RED_PB != ACTIVE) & (RED_PB != ACTIVE) & (RED_PB != ACTIVE) & (RED_PB != ACTIVE))
+		while((Read_PortB(RED_PB) != ACTIVE) & (Read_PortB(GRN_PB) != ACTIVE) & (Read_PortB(BLU_PB) != ACTIVE) & (Read_PortB(YEL_PB) != ACTIVE))
 		{
-			if (RED_PB == ACTIVE);
+			Delay_ms(5);
+		}
+
+
+			if (Read_PortB(RED_PB) == ACTIVE)
 			{
-				Repeatpattern[loop_ctr] = 'R';
+				game_data->pattern[loop_ctr] = 'R';
 				Write_PortB(RED_LT, 1);
 				Write_PortB(GRN_LT, 0);
 				Write_PortB(BLU_LT, 0);
 				Write_PortB(YEL_LT, 0);
-				break;
 			}
-
-			if (GRN_PB == ACTIVE);
+			else if (Read_PortB(GRN_PB) == ACTIVE)
 			{
-				Repeatpattern[loop_ctr] = 'G';
+				game_data->pattern[loop_ctr] = 'G';
 				Write_PortB(RED_LT, 0);
 				Write_PortB(GRN_LT, 1);
 				Write_PortB(BLU_LT, 0);
 				Write_PortB(YEL_LT, 0);
-				break;
 			}
-
-			if (BLU_PB == ACTIVE);
+			else if (Read_PortB(BLU_PB) == ACTIVE)
 			{
-				Repeatpattern[loop_ctr] = 'B';
+				game_data->pattern[loop_ctr] = 'B';
 				Write_PortB(RED_LT, 0);
 				Write_PortB(GRN_LT, 0);
 				Write_PortB(BLU_LT, 1);
 				Write_PortB(YEL_LT, 0);
-				break;
 			}
-
-			if (YEL_PB == ACTIVE);
+			else if (Read_PortB(YEL_PB) == ACTIVE)
 			{
-				Repeatpattern[loop_ctr] = 'Y';
+				game_data->pattern[loop_ctr] = 'Y';
 				Write_PortB(RED_LT, 0);
 				Write_PortB(GRN_LT, 0);
 				Write_PortB(BLU_LT, 0);
 				Write_PortB(YEL_LT, 1);
-				break;
+			}
+			else
+			{
+				//
 			}
 
 			// Hold to show saved color.
 			Delay_ms(250);
 
 			// Check if the new input matches the correct pattern
-			if (Repeatpattern[loop_ctr] != game_data.pattern[loop_ctr])
+			if (Repeatpattern[loop_ctr] != game_data->pattern[loop_ctr])
 			{ 
-				game_data.round_num = 0;
+				game_data->round_num = 0;
 				break;
 			}
 
-		}
 
 		// Game is over, player lost
-		if (game_data.round_num == 0)
+		if (game_data->round_num == 0)
 		{
 			// Transmit to other side that the game is over and that they won
-			game_data.winner = 1;
-			TxPattern();
-			game_data.winner = 2;
+			game_data->winner = 1;
+			TxPattern(game_data);
+			game_data->winner = 2;
 		}
 
 	}
 
 }
+
 
 // Sleep For The Specified Number Of Milliseconds
 void Delay_ms(int ms)
